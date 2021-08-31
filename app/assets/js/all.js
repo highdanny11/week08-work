0ececf6acdaa93c877849735f5267037eeabde24
highdanny11 <highdanny11@gmail.com>
2021-08-31 16:31:50 +0800

    增加hover

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -45,6 +45,10 @@ var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
